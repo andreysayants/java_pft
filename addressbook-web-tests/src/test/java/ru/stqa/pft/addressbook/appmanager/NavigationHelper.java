d67f5c1a2e242284a8dbe914acd482d58449c10a
Andrey <andrey.sayants@gmail.com>
2016-04-26 10:20:59 +0300

    Revert "Создаём общий базовый класс для классов-помощников"
    This reverts commit cf1fc4fc262f238ce445407789a9085d41b23f4a.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/NavigationHelper.java
@@ -6,13 +6,14 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by andrey.sayants on 25.04.2016.
  */
-public class NavigationHelper extends HelperBase {
+public class NavigationHelper {
+  private FirefoxDriver wd;
 
   public NavigationHelper(FirefoxDriver wd) {
-    super(wd);
+    this.wd = wd;
   }
 
   public void gotoGroupPage() {
-    click(By.linkText("groups"));
+    wd.findElement(By.linkText("groups")).click();
   }
 }
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/SessionHelper.java
@@ -6,15 +6,20 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 /**
  * Created by andrey.sayants on 25.04.2016.
  */
-public class SessionHelper extends HelperBase {
+public class SessionHelper {
+  private FirefoxDriver wd;
 
   public SessionHelper(FirefoxDriver wd) {
-    super(wd);
+    this.wd = wd;
   }
 
   public void login(String username, String password) {
-    type(By.name("user"), username);
-    type(By.name("pass"), password);
-    click(By.xpath("//form[@id='LoginForm']/input[3]"));
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
   }
 }
