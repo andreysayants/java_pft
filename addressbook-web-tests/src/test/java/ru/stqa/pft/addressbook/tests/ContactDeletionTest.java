e4aec5cb732905bfdfe7701c1fae90ee166b7528
Andrey <andrey.sayants@gmail.com>
2016-04-28 15:13:38 +0300

    Проверка и обеспечение выполнения предусловий 2

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactDeletionTest.java
@@ -1,12 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 
 public class ContactDeletionTest extends TestBase {
 
   @Test
   public void testContactDeletion() {
     app.getNavigationHelper().gotoContactPage();
+    if (!app.getContactHelper().isThereContact()) {
+      app.getContactHelper().createContact(new ContactData("first1", null, null, "address1", "89111111111", "test1"), true);
+    }
     app.getContactHelper().editContact();
     app.getContactHelper().deleteSelectedContact();
     app.getContactHelper().returnToHomePage();
