806a16c5ff7b50f652cf8b05ba9bd78fbc3a5c66
Andrey <andrey.sayants@gmail.com>
2016-05-06 12:30:37 +0300

    4.2 Коллекции наборы элементов (Массивы и Списки)

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Collections.java
@@ -0,0 +1,25 @@
+package ru.stqa.pft.sandbox;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by andrey.sayants on 06.05.2016.
+ */
+public class Collections {
+
+  public static void main(String[] args) {
+    String[] langs = {"Java", "C#", "Python", "PHP"};
+
+    List<String> languages = Arrays.asList("Java", "C#", "Python", "PHP");
+
+    for (String l : languages) {
+      System.out.println("Я хочу выучить " + l);
+    }
+
+    for (int i = 0; i < languages.size(); i++) {
+      System.out.println("Я хочу выучить " + languages.get(i));
+    }
+  }
+}
