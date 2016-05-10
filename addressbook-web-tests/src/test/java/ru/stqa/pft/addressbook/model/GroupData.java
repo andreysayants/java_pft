73f55fa09e4fd8f2fe319d47225965e5c45f929d
Andrey <andrey.sayants@gmail.com>
2016-05-10 15:08:27 +0300

    Сортировка списков для GroupCreation/ModificationTests

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/GroupData.java
@@ -6,13 +6,6 @@ public class GroupData {
   private final String header;
   private final String footer;
 
-  public GroupData(String name, String header, String footer) {
-    this.id = 0;
-    this.name = name;
-    this.header = header;
-    this.footer = footer;
-  }
-
   public GroupData(int id, String name, String header, String footer) {
     this.id = id;
     this.name = name;
@@ -54,15 +47,21 @@ public class GroupData {
 
     GroupData groupData = (GroupData) o;
 
-    if (id != groupData.id) return false;
     return name != null ? name.equals(groupData.name) : groupData.name == null;
 
   }
 
   @Override
   public int hashCode() {
-    int result = id;
-    result = 31 * result + (name != null ? name.hashCode() : 0);
-    return result;
+    return name != null ? name.hashCode() : 0;
   }
+
+  public GroupData(String name, String header, String footer) {
+    this.id = Integer.MAX_VALUE;
+    this.name = name;
+    this.header = header;
+    this.footer = footer;
+  }
+
+
 }
