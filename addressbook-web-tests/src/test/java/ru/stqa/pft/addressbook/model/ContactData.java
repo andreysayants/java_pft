1f07a093faa2ce8a96ffd00bea63ceec6ada0c81
Andrey <andrey.sayants@gmail.com>
2016-05-20 19:00:22 +0300

    Пути к файлам и директориям

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -1,5 +1,7 @@
 package ru.stqa.pft.addressbook.model;
 
+import java.io.File;
+
 public class ContactData {
   private int id = Integer.MAX_VALUE;
   private String firstname;
@@ -17,6 +19,16 @@ public class ContactData {
   private String Email2;
   private String Email3;
   private String info;
+  private File photo;
+
+  public File getPhoto() {
+    return photo;
+  }
+
+  public ContactData withPhoto(File photo) {
+    this.photo = photo;
+    return this;
+  }
 
   public String getInfo() {
     return info;
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTest.java
@@ -6,6 +6,8 @@ import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
 
+import java.io.File;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -21,8 +23,9 @@ public class ContactCreationTest extends TestBase {
 
   @Test
   public void testContactCreation() {
+    File photo = new File("src/test/resources/stru.png");
     ContactData contact = new ContactData()
-            .withFirstname("first1").withLastname("last2").withAddress("address1").withMobilePhone("89111111111").withGroup("test1");
+            .withFirstname("first3").withLastname("last2").withAddress("address1").withMobilePhone("89111111111").withGroup("test1").withPhoto(photo);
     app.goTo().ContactPage();
     Contacts before = app.Contact().All();
     app.Contact().create(contact);
@@ -32,4 +35,14 @@ public class ContactCreationTest extends TestBase {
     assertThat(after, equalTo(
             before.withAdded(contact.withId(after.stream().mapToInt((c) -> c.getId()).max().getAsInt()))));
   }
+
+ /* @Test
+  public void testCurrentDir() {
+    File currentDir = new File(".");
+    System.out.println(currentDir.getAbsolutePath());
+
+    File photo = new File("src/test/resources/stru.png");
+    System.out.println(photo.getAbsolutePath());
+    System.out.println(photo.exists());
+  }*/
 }
